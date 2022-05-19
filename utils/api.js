aefd086b9c7d2b6976c537eedce3d318a94d601d
Elcid-Chawa <Elcid-Chawa@users.noreply.github.com>
2022-05-20 00:23:51 +0100

    Add files via upload

--- utils/api.js
+++ utils/api.js
@@ -1,1 +1,40 @@
+const api = [
+    {
+        id: 1,
+        street:"Careffour Obili",
+        apt: "Apt 213"
+    },
+    {
+        id: 2,
+        street:"Careffour Jouvance",
+        apt: "Apt 214"
+    },
+    {
+        id: 3,
+        street:"GRA BOta",
+        apt: "Apt 215"
+    },
+    {
+        id: 4,
+        street:"New Town Junction",
+        apt: "Apt 216"
+    },
+    {
+        id: 5,
+        street:"Post Centrale",
+        apt: "Apt 216" 
+    },
+    {
+        id: 6,
+        street:"Johnson Street",
+        apt: "Apt 217"
+    },
+    {
+        id: 7,
+        street:"Open quater",
+        apt: "Apt 218"
+    },
 
+]
+
+module.exports = api;
